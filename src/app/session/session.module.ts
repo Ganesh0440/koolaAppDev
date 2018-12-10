58d3bed7b002309193f96201b3f413013de7c6d1
kiran5506 <chinthadakirankumar@gmail.com>
2018-12-10 16:55:04 +0530

    add footer saparately

--- src/app/session/session.module.ts
+++ src/app/session/session.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SessionComponent } from './session.component';
 import { SessionRoutingModule } from './session-routing.module';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule,
     SessionRoutingModule
   ],
-  declarations: [SessionComponent]
+  declarations: [SessionComponent, FooterComponent]
 })
 export class SessionModule { }
